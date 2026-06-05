f570193f5798805643c2e01210617c6bf6f48cdd
idohoumoubarack <Idohoumoubarack@gmail.com>
2026-06-05 11:42:27 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
   <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,400;1,600&family=Inter:wght@300;400;500&family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
 
   {{-- CSS --}}
-  <link rel="stylesheet" href="'css/main.css">
+  <link rel="stylesheet" href="css/main.css">
 
   <script src="https://unpkg.com/lucide@latest/dist/umd/lucide.min.js"></script>
   @stack('styles')
